958023604be9f1a8288da72b4e01c7cee0ca0ed1
IHprogram <ihm20202020@gmail.com>
2021-10-21 09:33:30 +0900

    corsの設定を変更

--- index.js
+++ index.js
@@ -8,9 +8,15 @@ import noteRoutes from './routes/notes.js';
 const app = express();
 const port = process.env.REACT_APP_PORT || 3002;
 
+const corsOptions = {
+  origin: 'http://localhost:3000',
+  credentials: true,
+  optionsSuccessStatus: 200
+}
+
 app.use(express.json({ limit: '50mb', extended: true }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
-app.use(cors({ origin: 'http://localhost:3000/', credentials: true, optionsSuccessStatus: 200 }));
+app.use(cors(corsOptions));
 
 app.use('/books', bookRoutes);
 app.use('/notes', noteRoutes);
