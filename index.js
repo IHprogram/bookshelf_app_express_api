a18161830f54427f9ffe1d62448c29d9a78c21cb
IHprogram <ihm20202020@gmail.com>
2021-10-25 11:57:29 +0900

    Access-Control-Allow-Originの設定を追加

--- index.js
+++ index.js
@@ -9,13 +9,13 @@ const app = express();
 const port = process.env.PORT || 3002;
 
 const corsOptions = {
-  origin: ['http://localhost:3000'],
+  origin: true,
   credentials: true,
 }
 
 app.use(express.json({ limit: '50mb', extended: true }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
-app.use(cors());
+app.use(cors(corsOptions));
 
 app.use('/books', bookRoutes);
 app.use('/notes', noteRoutes);
@@ -24,3 +24,8 @@ mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => app.listen(port, () => {
     console.log(`${port}に接続しました`)
   }))
+
+// ドメイン直下にアクセスしたときのCORS許可
+app.get('/mypage', (req, res, next) => {
+  res.set({ 'Access-Control-Allow-Origin': '*' });
+});
